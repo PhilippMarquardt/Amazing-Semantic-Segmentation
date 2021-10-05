26ba0c6b76e8e936caf817126ddb3e79c9dc3385
PhilippMarquardt <p.marquardt15@googlemail.com>
2021-10-05 10:15:41 +0200

    .

---
+++ build/lib/amazingbase_models/__init__.py
@@ -0,0 +1,5 @@
+from amazingbase_models.densenet import DenseNet
+from amazingbase_models.resnet import ResNet
+from amazingbase_models.vgg import VGG
+from amazingbase_models.xception import Xception
+from amazingbase_models.mobilenet import MobileNet
